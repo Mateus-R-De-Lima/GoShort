dc5de8f3570b35566d1b27b018d73e5908756d8a
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-04-17 09:22:28 -0300

    feature - Implementando logica de post e get.

--- api/api.go
+++ api/api.go
@@ -2,7 +2,11 @@ package api
 
 //go get github.com/go-chi/chi/v5 -> Comando para instalar pacote chi
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand"
 	"net/http"
+	"net/url"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -17,7 +21,7 @@ type Response struct {
 	URL   string `json:"url,omitempty"`
 }
 
-func NewHandler() http.Handler {
+func NewHandler(db map[string]string) http.Handler {
 
 	r := chi.NewMux()
 
@@ -26,16 +30,87 @@ func NewHandler() http.Handler {
 	r.Use(middleware.RequestID)
 
 	// Rotas
-	r.Post("/shorten", handlePost)
-	r.Get("/{id}", handleGet)
+	r.Post("/shorten", handlePost(db))
+	r.Get("/{code}", handleGet(db))
 
 	return r
 }
+func sendJSON(w http.ResponseWriter, resp Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err)
+		sendJSON(
+			w,
+			Response{Error: "something went wrong"},
+			http.StatusInternalServerError,
+		)
+		return
+	}
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
-
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write json data", "error", err)
+		return
+	}
 }
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+func genCode() string {
+	const n = 8
+	byts := make([]byte, n)
+	for i := range n {
+		byts[i] = characters[rand.Intn(len(characters))]
+	}
+	return string(byts)
+}
+
+func handlePost(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			slog.Error("failed to decode request body", "error", err)
+			sendJSON(
+				w,
+				Response{Error: "invalid request body"},
+				http.StatusBadRequest,
+			)
+			return
+		}
+		if _, err := url.Parse(body.URL); err != nil {
+			slog.Error("invalid url", "error", err)
+			sendJSON(
+				w,
+				Response{Error: "invalid url"},
+				http.StatusBadRequest,
+			)
+			return
+		}
+
+		code := genCode()
+		db[code] = body.URL
+		sendJSON(
+			w,
+			Response{URL: body.URL},
+			http.StatusCreated,
+		)
+	}
+}
+
+func handleGet(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := chi.URLParam(r, "code")
+		data, ok := db[code]
+		if !ok {
+			slog.Info("code not found", "code", code)
+			sendJSON(
+				w,
+				Response{Error: "code not found"},
+				http.StatusNotFound,
+			)
+			return
+		}
+		http.Redirect(w, r, data, http.StatusFound)
+	}
 }
--- man.go
+++ man.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"goshort/api"
 	"log/slog"
+	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -14,6 +17,21 @@ func main() {
 }
 
 func run() error {
+	db := make(map[string]string)
+
+	handler := api.NewHandler(db)
+
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
 
 	return nil
 
